9c3d4e3351b75fd057be9006f5ef2a9f8308e0f3
Adnya <adnyadawkhar2005@gmail.com>
2025-11-07 05:30:50 +0000

    Added and styled blog list and blog item

--- src/components/BlogItem/index.js
+++ src/components/BlogItem/index.js
@@ -1,1 +1,18 @@
-// Write your JS code here
+import './index.css'
+
+const BlogItem = props => {
+  const {itemDetail} = props
+  const {title, description, publishedDate} = itemDetail
+  return (
+    <li className="eachItem">
+      <div className="name-date-container">
+        <h2 className="title">{title}</h2>
+        <p className="date">{publishedDate}</p>
+      </div>
+      <p className="description">{description}</p>
+      <hr className="hr-line" />
+    </li>
+  )
+}
+
+export default BlogItem
--- src/components/BlogList/index.js
+++ src/components/BlogList/index.js
@@ -1,1 +1,15 @@
-// Write your JS code here
+import './index.css'
+import BlogItem from '../BlogItem'
+
+const BlogList = props => {
+  const {blogsList} = props
+  return (
+    <ul className="blog-container">
+      {blogsList.map(eachItem => (
+        <BlogItem key={eachItem.id} itemDetail={eachItem} />
+      ))}
+    </ul>
+  )
+}
+
+export default BlogList
